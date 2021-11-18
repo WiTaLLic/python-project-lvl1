cb08bbab13314ad8d42b4add6789895fc1561627
navichec@mail.ru <navichec@mail.ru>
2021-11-18 22:13:20 +0300

    modified brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+from brain_games.cli import welcom_user
 def main():
     print("Welcom to the Brain Games!")
+    welcom_user()
 
 if __name__ == '__main__':
-    main()
-	
+    main()	
+
+
