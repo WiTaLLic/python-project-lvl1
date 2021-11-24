4f8feb4ef9ea66f6f95a29b701b725338de16be8
navichec@mail.ru <navichec@mail.ru>
2021-11-24 15:46:13 +0300

    modified cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,7 @@
 import prompt
 
+
 def welcom_user():
-	"""The function asks for user data"""
-	name = prompt.string('May I have your name? ')
-	print(f"Hello, {name}!")
+    """The function asks for user data"""
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
