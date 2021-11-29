56492209c0da51c7453137fc68ecad96e151eddb
navichec@mail.ru <navichec@mail.ru>
2021-11-29 10:56:22 +0300

    modified brain-even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from brain_games.salute import welcom_user
+
+
+def main():
+    print("Welcom to the Brain Games!")
+    welcom_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+if __name__ == '__main__':
+    main()
