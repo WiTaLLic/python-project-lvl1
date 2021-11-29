62ab2d4da17a5c7ade90f26ba95f6e63f5faf780
navichec@mail.ru <navichec@mail.ru>
2021-11-29 09:13:32 +0300

    new file brain_game.py

---
+++ brain_games/scripts/brain_game.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from brain_games.cli import welcom_user
+
+
+def main():
+    print("Welcom to the Brain Games!")
+    welcom_user()
+
+
+if __name__ == '__main__':
+    main()
