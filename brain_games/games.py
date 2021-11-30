23eb860622f29002f91a139ead87a698021f7627
navichec@mail.ru <navichec@mail.ru>
2021-11-30 22:33:11 +0300

    modified games.py

--- brain_games/games.py
+++ brain_games/games.py
@@ -5,22 +5,21 @@ from random import randint
 
 
 def game(NUMBER_QUESTIONS, names):
-    """"""
+    """ Start game """
     random_number = randint(0, 100)
     
-    
+    if NUMBER_QUESTIONS < 1:
+        print(f"Congratulations, {names}!")
+        return
 
     NUMBER_QUESTIONS -= 1
 
     question(random_number)
-
     respons = plaer_respons()
-    
     
     if random_number % 2 != 0 and respons == 'yes':
         print(f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {names}!")
         return
-        
     elif random_number % 2 == 0 and respons == 'no':
         print(f"'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {names}!")
         return
