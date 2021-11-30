e372fdb2bc712fd1ddb12fcd24e75410923281ac
navichec@mail.ru <navichec@mail.ru>
2021-12-01 01:28:16 +0300

    modified brain-even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,17 +2,19 @@
 from brain_games.salute import welcom_user
 from brain_games.games import game
 
- 
-NUMBER_QUESTIONS = 3 
+
+NUMBER_QUESTIONS = 3
+
 
 def main():
     print("Welcom to the Brain Games!")
 
     names = welcom_user()
-    
+
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
     game(NUMBER_QUESTIONS, names)
+
 
 if __name__ == '__main__':
     main()
