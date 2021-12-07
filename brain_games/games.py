6c1dc648124f1be1dbee2882b50a5c4eb3fe9100
navichec@mail.ru <navichec@mail.ru>
2021-12-07 22:41:43 +0300

    modified games.py added game_calc

--- brain_games/games.py
+++ brain_games/games.py
@@ -1,7 +1,10 @@
 from brain_games.quest import question
+from brain_games.quest import question_calc
 from brain_games.respons import plaer_respons
 from brain_games.review import reviews
-from random import randint
+from brain_games.review import reviews_calc
+from brain_games.math_operation import operation
+from random import choice, randint
 
 
 def game(NUMBER_QUESTIONS, names):
@@ -35,3 +38,35 @@ def game(NUMBER_QUESTIONS, names):
         return
     reviews(random_number, respons)
     game(NUMBER_QUESTIONS, names)
+
+
+def game_calc(NUMBER_QUESTIONS, names):
+    """ Start game calculator """
+    random_number_one = randint(0, 100)
+    random_math_operator = choice('+-*')
+    random_number_two = randint(0, 100)
+
+    if NUMBER_QUESTIONS < 1:
+        print(f"Congratulations, {names}!")
+        return
+
+    NUMBER_QUESTIONS -= 1
+
+    question_calc(
+        random_number_one,
+        random_math_operator,
+        random_number_two
+    )
+
+    respons = plaer_respons()
+    calc_result = operation(
+        random_number_one,
+        random_math_operator,
+        random_number_two
+    )
+    correct_input = reviews_calc(respons, calc_result, names)
+
+    if correct_input is not True:
+        return
+
+    game_calc(NUMBER_QUESTIONS, names)
