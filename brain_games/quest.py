811f1815d0eb8976badf188f8c14dcfc4571d98a
navichec@mail.ru <navichec@mail.ru>
2021-12-07 22:42:51 +0300

    modified quest.py added question_calc

--- brain_games/quest.py
+++ brain_games/quest.py
@@ -1,2 +1,9 @@
 def question(random_number):
+    """Random choice of question """
     print(f"Question: {random_number}")
+
+
+def question_calc(random_number_one, random_math_operator, random_number_two):
+    """Random selection of two operands and an operator"""
+    print(f"Question: {random_number_one} \
+{random_math_operator} {random_number_two}")
