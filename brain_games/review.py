d5d6599a5e06d7a03f77b684c48effe25fc210dd
navichec@mail.ru <navichec@mail.ru>
2021-12-07 22:44:01 +0300

    modified review.py added reviews_calc

--- brain_games/review.py
+++ brain_games/review.py
@@ -4,3 +4,14 @@ def reviews(random_number, respons):
         print('Correct!')
     elif random_number % 2 != 0 and respons == 'no':
         print('Correct!')
+
+
+def reviews_calc(respons, calc_result, names):
+    """Сorrect answer"""
+    if respons == str(calc_result):
+        print('Correct!')
+        return True
+    else:
+        print(f"'{respons}' is  wrong answer ;(. Correct answer was '{calc_result}'.\
+            \nLet's try again, {names}!")
+        return False
