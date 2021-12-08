2b42c4b50885f0181583a1e2099546d0282805d9
navichec@mail.ru <navichec@mail.ru>
2021-12-08 08:51:38 +0300

    modified math_operation.py added operation_gcd

--- brain_games/math_operation.py
+++ brain_games/math_operation.py
@@ -7,3 +7,13 @@ def operation(random_number_one, random_math_operator, random_number_two):
     if random_math_operator == '*':
         calculation_result = random_number_one * random_number_two
     return (calculation_result)
+
+
+def operation_gcd(random_number_one, random_number_two):
+    """Search for gcd of two numbers """
+    if(random_number_two == 0):
+        return random_number_one
+    else:
+        return operation_gcd(
+            random_number_two,
+            random_number_one % random_number_two)
