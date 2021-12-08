2aa97562c8e28c7986b30244eea38ab13f9e6afb
navichec@mail.ru <navichec@mail.ru>
2021-12-08 08:50:25 +0300

    modified conditions_games.py added condition_gcd

--- brain_games/conditions_games.py
+++ brain_games/conditions_games.py
@@ -1,6 +1,13 @@
 def conditions_even():
+    """Сonditions of the game-even"""
     print('Answer "yes" if the number is even, otherwise answer "no"')
 
 
 def conditions_calc():
+    """Сonditions of the game-calc"""
     print('What is the result of the expression?')
+
+
+def condition_gcd():
+    """Сonditions of the game-gcd"""
+    print('Find the greatest common divisor of given numbers.')
