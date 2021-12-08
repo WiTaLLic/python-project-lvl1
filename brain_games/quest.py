a0ebbe7b736388967c7494ba7cd523ea71de8e8d
navichec@mail.ru <navichec@mail.ru>
2021-12-08 23:52:46 +0300

    added question_progression

--- brain_games/quest.py
+++ brain_games/quest.py
@@ -12,3 +12,13 @@ def question_calc(random_number_one, random_math_operator, random_number_two):
 def question_gcd(random_number_one, random_number_two):
     """Сreate two random numbers to find the gcd """
     print(f"Question: {random_number_one} {random_number_two}")
+
+
+def question_prograssion(random_START, STOP, random_STEP, hidden_number):
+    """Аrithmetic progression output """
+    print('Question: ', end=' ')
+    for i in range(random_START, STOP, random_STEP):
+        if i == hidden_number:
+            print('..', end=' ')
+        else:
+            print(i, end=' ')
