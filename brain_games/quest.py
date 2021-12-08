35327a5926ed252438ebe20a1edff7b6ddd75a13
navichec@mail.ru <navichec@mail.ru>
2021-12-08 08:49:07 +0300

    modified qest.py added question_gcd

--- brain_games/quest.py
+++ brain_games/quest.py
@@ -5,5 +5,10 @@ def question(random_number):
 
 def question_calc(random_number_one, random_math_operator, random_number_two):
     """Random selection of two operands and an operator"""
-    print(f"Question: {random_number_one} \
-{random_math_operator} {random_number_two}")
+    print(f"Question: {random_number_one} {random_math_operator} \
+    {random_number_two}")
+
+
+def question_gcd(random_number_one, random_number_two):
+    """Сreate two random numbers to find the gcd """
+    print(f"Question: {random_number_one} {random_number_two}")
