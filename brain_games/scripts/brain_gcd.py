15c2b289e8b9bc654576a036e5a6a2e5adbc09db
navichec@mail.ru <navichec@mail.ru>
2021-12-15 00:46:44 +0300

    modified brain_gcd

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,21 +1,27 @@
-#!/usr/bin/env python
-from brain_games.salute import welcom_user
-from brain_games.games import game_gcd
-from brain_games.conditions_games import condition_gcd
+from brain_games.games.quest import question_gcd
+from brain_games.games.respons import plaer_respons
+from brain_games.games.review import reviews_calc
+from brain_games.games.math_operation import operation_gcd
+from random import randint
 
 
-NUMBER_QUESTIONS = 3
+def game_gcd(NUMBER_QUESTIONS, names):
+    """ Start game-gcd """
+    random_number_one = randint(0, 100)
+    random_number_two = randint(0, 100)
 
+    if NUMBER_QUESTIONS < 1:
+        print(f"Congratulations, {names}!")
+        return
 
-def main():
-    print("Welcom to the Brain Games!")
+    NUMBER_QUESTIONS -= 1
 
-    names = welcom_user()
+    question_gcd(random_number_one, random_number_two)
+    respons = plaer_respons()
+    gcd_result = operation_gcd(random_number_one, random_number_two)
+    correct_input = reviews_calc(respons, gcd_result, names)
 
-    condition_gcd()
+    if correct_input is not True:
+        return
 
     game_gcd(NUMBER_QUESTIONS, names)
-
-
-if __name__ == '__main__':
-    main()
