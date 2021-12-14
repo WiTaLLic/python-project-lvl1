162e77c7b1577a0f9ee31e6bcf4e10210bcf9004
navichec@mail.ru <navichec@mail.ru>
2021-12-15 00:45:20 +0300

    modified brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,21 +1,38 @@
-#!/usr/bin/env python
-from brain_games.salute import welcom_user
-from brain_games.games import game_calc
-from brain_games.conditions_games import conditions_calc
+from brain_games.games.quest import question_calc
+from brain_games.games.respons import plaer_respons
+from brain_games.games.review import reviews_calc
+from brain_games.games.math_operation import operation
+from random import choice, randint
 
 
-NUMBER_QUESTIONS = 3
+def game_calc(NUMBER_QUESTIONS, names):
+    """ Start game-calc """
+    random_number_one = randint(0, 100)
+    random_math_operator = choice('+-*')
+    random_number_two = randint(0, 100)
 
+    if NUMBER_QUESTIONS < 1:
+        print(f"Congratulations, {names}!")
+        return
 
-def main():
-    print("Welcom to the Brain Games!")
+        
+    NUMBER_QUESTIONS -= 1
 
-    names = welcom_user()
+    question_calc(
+        random_number_one,
+        random_math_operator,
+        random_number_two
+    )
 
-    conditions_calc()
+    respons = plaer_respons()
+    calc_result = operation(
+        random_number_one,
+        random_math_operator,
+        random_number_two
+    )
+    correct_input = reviews_calc(respons, calc_result, names)
+
+    if correct_input is not True:
+        return
 
     game_calc(NUMBER_QUESTIONS, names)
-
-
-if __name__ == '__main__':
-    main()
