0b7e75c1ad9706ed3d20f4e1538a371984824da7
navichec@mail.ru <navichec@mail.ru>
2021-12-15 00:40:22 +0300

    modified salute

--- brain_games/games/salute.py
+++ brain_games/games/salute.py
@@ -1,6 +1,10 @@
 import prompt
 
 
+def welcome_to_game():
+    print("Welcom to the Brain Games!")
+
+
 def welcom_user():
     """The function asks for user data"""
     name = prompt.string('May I have your name? ')
