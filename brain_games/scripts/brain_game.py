5385906a7f38404c24311e8406c80126bd19e270
navichec@mail.ru <navichec@mail.ru>
2022-01-07 02:38:17 +0300

    modified brain-game

--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from brain_games.cli import welcom_user
 
+
 def welcom_to_game():
     print('Welcome to the Brain Games!')
+
 
 def main():
     welcom_to_game()
     welcom_user()
+
 
 if __name__ == '__main__':
     main()
