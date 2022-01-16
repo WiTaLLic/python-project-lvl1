93fd20cae7f71b1d807e5e40ad13eb3bb62a24fe
navichec@mail.ru <navichec@mail.ru>
2022-01-17 01:19:39 +0300

    modified brain-calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
-from brain_games.games.calc import welcom_to_game
-from brain_games.games.calc import welcom_user
-from brain_games.games.calc import conditions_calc
-from brain_games.games.calc import game_calc
-from brain_games.games.even import NUMBER_QUESTIONS
+from brain_games.engine import start
 
 
 def main():
-    welcom_to_game()
-    name = welcom_user()
-    conditions_calc()
-
-    game_calc(NUMBER_QUESTIONS, name)
-
+    start(game = 'calc')
+    
 
 if __name__ == '__main__':
     main()
